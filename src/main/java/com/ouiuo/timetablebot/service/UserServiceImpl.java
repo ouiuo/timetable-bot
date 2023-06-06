69053cb10a6bc52fc9b2bf01bd2baa3ba2ee2d49
Дегтярев Василий Александрович <vdegtyarev@cbgr.ru>
2023-06-07 00:21:26 +0300

    fix - auto create Users table if not exists

--- src/main/java/com/ouiuo/timetablebot/service/UserServiceImpl.java
+++ src/main/java/com/ouiuo/timetablebot/service/UserServiceImpl.java
@@ -2,17 +2,17 @@ package com.ouiuo.timetablebot.service;
 
 import com.ouiuo.timetablebot.dao.UsersRepository;
 import com.ouiuo.timetablebot.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UsersRepository usersRepository;
-
-    public UserServiceImpl(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
 
     @Override
     public void updateOnline(org.telegram.telegrambots.meta.api.objects.User tUser) {
